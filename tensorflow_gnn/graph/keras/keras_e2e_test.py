7d56f102eb809a94b1889e1a6997e3827e5a038a
Graph Learning Team <no-reply@google.com>
2021-10-18 17:32:39 +0000

    Export the redesigned tfgnn.keras.layers.GraphUpdate and related classes.
    PiperOrigin-RevId: 403997156

--- tensorflow_gnn/graph/keras/keras_e2e_test.py
+++ tensorflow_gnn/graph/keras/keras_e2e_test.py
@@ -29,6 +29,15 @@ class ExportedKerasNamesTest(tf.test.TestCase):
     self.assertIsSubclass(tfgnn.keras.layers.Pool, Layer)
     self.assertIsSubclass(tfgnn.keras.layers.Readout, Layer)
     self.assertIsSubclass(tfgnn.keras.layers.ReadoutFirstNode, Layer)
+    self.assertIsSubclass(tfgnn.keras.layers.ConvolutionFromEdgeSetUpdate,
+                          Layer)
+    self.assertIsSubclass(tfgnn.keras.layers.SimpleConvolution, Layer)
+    self.assertIsSubclass(tfgnn.keras.layers.NextStateFromConcat, Layer)
+    self.assertIsSubclass(tfgnn.keras.layers.ResidualNextState, Layer)
+    self.assertIsSubclass(tfgnn.keras.layers.EdgeSetUpdate, Layer)
+    self.assertIsSubclass(tfgnn.keras.layers.NodeSetUpdate, Layer)
+    self.assertIsSubclass(tfgnn.keras.layers.ContextUpdate, Layer)
+    self.assertIsSubclass(tfgnn.keras.layers.GraphUpdate, Layer)
 
 
 # An example of a custom Keras layer used by tests below.
@@ -65,6 +74,30 @@ class AddWeightedSwappedInEdges(tf.keras.layers.Layer):
     node_state += node_updates
     return graph.replace_features(
         node_sets={'node': {'hidden_state': node_state}})
+
+
+# A similar example of model building with tfgnn.keras.layers.*.
+def add_weighted_swapped_in_edges(graph):
+  def _source_times_weight(inputs):
+    edge_inputs, node_inputs, _ = inputs
+    return tf.multiply(edge_inputs['edge_weight'], node_inputs[tfgnn.SOURCE])
+  graph = tfgnn.keras.layers.GraphUpdate(
+      edge_sets={
+          'edge': tfgnn.keras.layers.EdgeSetUpdate(
+              tf.keras.layers.Lambda(_source_times_weight),
+              edge_input_feature=['edge_weight'],
+              node_input_tags=[tfgnn.SOURCE])},
+      node_sets={
+          'node': tfgnn.keras.layers.NodeSetUpdate(
+              {'edge': tfgnn.keras.layers.Pool(tfgnn.TARGET, 'sum')},
+              tfgnn.keras.layers.NextStateFromConcat(
+                  tf.keras.layers.Dense(
+                      units=2, name='add_swapped_message', use_bias=False,
+                      kernel_initializer=tf.keras.initializers.Constant(
+                          [[1., 0., 0., 1.],
+                           [0., 1., 1., 0.]]))))}
+  )(graph)
+  return graph
 
 
 class GraphTensorKerasModelTest(tf.test.TestCase, parameterized.TestCase):
@@ -140,6 +173,26 @@ class GraphTensorKerasModelTest(tf.test.TestCase, parameterized.TestCase):
     self.assertAllEqual(spec.edge_sets_spec['edge']['edge_weight'],
                         tf.TensorSpec(tf.TensorShape([None, 1]), tf.float32))
     return spec
+
+  @parameterized.parameters([True, False])
+  def testStdLayerModel(self, static_shapes):
+
+    # A Keras Model build from tfgnn.keras.layers.*.
+    inputs = tf.keras.layers.Input(
+        type_spec=self._get_input_spec(static_shapes))
+    graph = add_weighted_swapped_in_edges(inputs)
+    outputs = tfgnn.keras.layers.Readout(node_set_name='node')(graph)
+    model = tf.keras.Model(inputs, outputs)
+
+    # Save and restore the model.
+    export_dir = os.path.join(self.get_temp_dir(), 'stdlayer-tf')
+    tf.saved_model.save(model, export_dir)
+    restored_model = tf.saved_model.load(export_dir)
+
+    expected_1 = as_tensor([[10., -6.], [12., 5.]], tf.float32)
+    graph_1 = self._create_graph_tensor(static_shapes, factor=1)
+    self.assertAllClose(model(graph_1), expected_1)
+    self.assertAllClose(restored_model(graph_1), expected_1)
 
   @parameterized.parameters([True, False])
   def testCustomGraphToGraphModel(self, static_shapes):
--- tensorflow_gnn/graph/keras/layers/__init__.py
+++ tensorflow_gnn/graph/keras/layers/__init__.py
@@ -1,6 +1,9 @@
 """The tfgnn.keras.layers package."""
 
+from tensorflow_gnn.graph.keras.layers import convolutions
 from tensorflow_gnn.graph.keras.layers import graph_ops
+from tensorflow_gnn.graph.keras.layers import graph_update
+from tensorflow_gnn.graph.keras.layers import next_state
 from tensorflow_gnn.graph.keras.layers.gat import gatv2
 
 Broadcast = graph_ops.Broadcast
@@ -8,9 +11,23 @@ Pool = graph_ops.Pool
 Readout = graph_ops.Readout
 ReadoutFirstNode = graph_ops.ReadoutFirstNode
 
+ConvolutionFromEdgeSetUpdate = convolutions.ConvolutionFromEdgeSetUpdate
+SimpleConvolution = convolutions.SimpleConvolution
+
+NextStateFromConcat = next_state.NextStateFromConcat
+ResidualNextState = next_state.ResidualNextState
+
+EdgeSetUpdate = graph_update.EdgeSetUpdate
+NodeSetUpdate = graph_update.NodeSetUpdate
+ContextUpdate = graph_update.ContextUpdate
+GraphUpdate = graph_update.GraphUpdate
+
 # GATv2 model.
 GATv2 = gatv2.GATv2
 
 # Prune imported module symbols so they're not accessible implicitly.
-del gatv2
+del convolutions
 del graph_ops
+del graph_update
+del next_state
+del gatv2
