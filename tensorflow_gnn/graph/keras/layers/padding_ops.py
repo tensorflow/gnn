2ceafc5e0dadb467b4863e9f835906032c7eecf6
arnoegw <arnoegw@google.com>
2022-03-28 10:09:50 +0000

    Add Keras wrappper PadToTotalSizes.
    PiperOrigin-RevId: 437710040

---
+++ tensorflow_gnn/graph/keras/layers/padding_ops.py
@@ -0,0 +1,50 @@
+"""Keras layer types for padding ops."""
+
+import tensorflow as tf
+
+from tensorflow_gnn.graph import padding_ops
+from tensorflow_gnn.graph import preprocessing_common
+
+
+@tf.keras.utils.register_keras_serializable(package="GNN")
+class PadToTotalSizes(tf.keras.layers.Layer):
+  """Applies tfgnn.pad_to_total_sizes() to a GraphTensor.
+
+  This Keras layer maps a GraphTensor to a GraphTensor by calling
+  `tfgnn.pad_to_total_sizes()` with the additional arguments, notably
+  `sizes_constraints`, passed at initialization time. See that function
+  for detailed documentation.
+
+  Serialization to a Keras model config requires the `sizes_constraints` to
+  contain Python integers or eager Tensors, not symbolic Tensors.
+  """
+
+  def __init__(self,
+               sizes_constraints: preprocessing_common.SizesConstraints,
+               *,
+               validate: bool = True,
+               **kwargs):
+    super().__init__(**kwargs)
+    self._sizes_constraints = sizes_constraints
+    self._validate = validate
+
+  def get_config(self):
+    try:
+      int_constraints = tf.nest.map_structure(int, self._sizes_constraints)
+    except TypeError as e:
+      raise NotImplementedError(  # Let SavedModel export skip this gracefully.
+          "get_config() requires sizes_constraints convertible to int.") from e
+    return dict(
+        sizes_constraints=int_constraints._asdict(),
+        validate=self._validate,
+        **super().get_config())
+
+  @classmethod
+  def from_config(cls, config):
+    sizes_constraints = preprocessing_common.SizesConstraints(
+        **config.pop("sizes_constraints"))
+    return cls(sizes_constraints=sizes_constraints, **config)
+
+  def call(self, graph):
+    return padding_ops.pad_to_total_sizes(
+        graph, self._sizes_constraints, validate=self._validate)
