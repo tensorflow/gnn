d593b48ea6b4b4b2ff008454a51d611941aa6ce8
bmayer <bmayer@google.com>
2022-04-18 18:54:51 +0000

    TF-GNN converter, examples and sampler.
    PiperOrigin-RevId: 442596038

--- setup.py
+++ setup.py
@@ -142,6 +142,8 @@ with open('README.md') as fp:
 
 
 console_scripts = [
+    'tensorflow_gnn.converters.ogb.convert_ogb_dataset',
+    'tensorflow_gnn.sampler.graph_sampler',
     'tensorflow_gnn.tools.generate_training_data',
     'tensorflow_gnn.tools.print_training_data',
     'tensorflow_gnn.tools.sampled_stats',
