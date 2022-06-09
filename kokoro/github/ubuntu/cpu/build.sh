d70fff816258d4dd2b7ec715e7a437e96a180f0e
mparadkar <mparadkar@google.com>
2022-06-09 09:24:01 -0700

    Attempt at fixing Kokoro build script
    PiperOrigin-RevId: 453942626

--- kokoro/github/ubuntu/cpu/build.sh
+++ kokoro/github/ubuntu/cpu/build.sh
@@ -30,17 +30,16 @@ source venv/bin/activate
 python --version
 python3 --version
 
-cd "src/github/tensorflow_gnn"
 TEST_ROOT=$(pwd)/${PIP_TEST_PREFIX}
 rm -rf "$TEST_ROOT"
 mkdir -p "$TEST_ROOT"
-ln -s "$(pwd)"/tensorflow "$TEST_ROOT"/tensorflow
+ln -s "$(pwd)"/tensorflow_gnn "$TEST_ROOT"/tensorflow_gnn
 
 bazel clean
 python3 setup.py bdist_wheel
 pip uninstall -y tensorflow_gnn
 pip install -r requirements-dev.txt
-pip install dist/tensorflow_gnn-0.1.0-py3-none-any.whl
+pip install dist/tensorflow_gnn-0.2.0.dev1-py3-none-any.whl
 
 bazel test --test_output=errors --verbose_failures=true --build_tests_only --define=no_tfgnn_py_deps=true --keep_going --experimental_repo_remote_exec //bazel_pip/tensorflow_gnn/...
 
