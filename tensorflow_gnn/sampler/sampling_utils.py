d155e58f886f3628e6c7aec38254a6d32c83cfc2
aferludin <aferludin@google.com>
2022-06-16 07:35:46 -0700

    Balanced inner lookup joins, combiner for unique values aggregation.
    PiperOrigin-RevId: 455379371

---
+++ tensorflow_gnn/sampler/sampling_utils.py
@@ -0,0 +1,120 @@
+"""Sampling-related utils."""
+
+import random
+from typing import Any, List, Optional, Iterable, Iterator, Tuple
+import apache_beam as beam
+
+Key = Any
+QueryData = Any
+ValueData = Any
+ShardedKey = Tuple[Key, int]
+PCollection = beam.pvalue.PCollection
+
+
+def unique_values_combiner(values: Iterable[List[Any]],
+                           max_result_size: Optional[int] = None) -> List[Any]:
+  """Extracts unique values from multiple input list.
+
+  Args:
+    values: Input lists.
+    max_result_size: The maximum result caridatinality, if applied. Used to
+      validate result.
+
+  Returns:
+    Unique values aggregated from input.
+
+  Raises:
+    ValueError: If result size if larger then `max_result_size`.
+  """
+  result = set()
+  for ids in values:
+    result.update(ids)
+  result = list(result)
+  if max_result_size is not None and len(result) > max_result_size:
+    raise ValueError(
+        f"Result size {len(result)} is larger than {max_result_size}.")
+
+  return result
+
+
+def balanced_inner_lookup_join(
+    stage_name: str,
+    queries: PCollection[Tuple[Key, QueryData]],
+    lookup_table: PCollection[Tuple[Key, ValueData]],
+    num_shards: int = 1024
+) -> PCollection[Tuple[Key, Tuple[QueryData, ValueData]]]:
+  """Matches keys from queries with unique values.
+
+  Results are returned only for matched keys.
+
+  Compared to the `CoGroupByKey` the implementation avoids materialization of
+  all queries having the same key on a single worker memory. This is achieved by
+  sharding the query keys into the `num_shards` buckets, duplicating values for
+  each key's shard and joining sharded queries with sharded values using
+  `(key, shard)` tuple. The approach allows to better load-balance hot keys
+  with the cost of introducing more stages.
+
+  Args:
+    stage_name: The unique Beam stage name.
+    queries: Pairs of query keys and associated query data.
+    lookup_table: Values keyed by unique keys.
+    num_shards: The total number of buckets used to shard input keys. This
+      parameter controls how hot keys are sharded.
+
+  Returns:
+    Pairs of query data and value for matched keys.
+
+  Raises:
+    ValueError: If value keys are not unique or number of shards is not
+      positive.
+  """
+  if num_shards <= 0:
+    raise ValueError("The number of shards should be positive,"
+                     f" got {num_shards}.")
+
+  def shard_fn(key: Key, data: QueryData) -> Tuple[ShardedKey, QueryData]:
+    return (key, random.randint(0, num_shards - 1)), data
+
+  def shard_values(key: Key, group) -> Iterator[Tuple[ShardedKey, ValueData]]:
+    for value_index, value in enumerate(group["value"]):
+      if value_index != 0:
+        raise ValueError(f"Values are not unique for key={key}.")
+
+      for shard_index, shards in enumerate(group["shards"]):
+        assert shard_index == 0, f"Shards are not unique for key={key}."
+        for shard in shards:
+          yield (key, shard), value
+
+  sharded_queries: PCollection[Tuple[ShardedKey, QueryData]] = (
+      queries | f"{stage_name}/ShardQueries" >> beam.MapTuple(shard_fn))
+
+  def as_list(key: Key, shard: int)-> Tuple[Key, List[int]]:
+    return key, [shard]
+
+  unique_shards: PCollection[Tuple[Key, List[int]]] = (
+      sharded_queries
+      | f"{stage_name}/ExtractShardedKeys" >> beam.Keys()
+      | f"{stage_name}/ConvertShardToList" >> beam.MapTuple(as_list)
+      | f"{stage_name}/CreateUniqueShards" >> beam.CombinePerKey(
+          unique_values_combiner, max_result_size=num_shards))
+  sharded_values: PCollection[Tuple[ShardedKey, ValueData]] = (
+      {
+          "shards": unique_shards,
+          "value": lookup_table
+      }
+      | f"{stage_name}/JoinValuesAndShards" >> beam.CoGroupByKey()
+      | f"{stage_name}/ShardValues" >> beam.FlatMapTuple(shard_values))
+
+  def extract_result(
+      sharded_key: ShardedKey,
+      group) -> Iterator[Tuple[Key, Tuple[QueryData, ValueData]]]:
+    for value_data in group["value"]:
+      for query_data in group["queries"]:
+        yield sharded_key[0], (query_data, value_data)
+
+  return ({
+      "queries": sharded_queries,
+      "value": sharded_values
+  }
+          | f"{stage_name}/JoinShardedQueriesAndValues" >> beam.CoGroupByKey()
+          | f"{stage_name}/ExtractResult" >> beam.FlatMapTuple(extract_result))
---
+++ tensorflow_gnn/sampler/sampling_utils_test.py
@@ -0,0 +1,117 @@
+"""Tests for sampling_utils."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+import apache_beam as beam
+from apache_beam.testing import util
+from tensorflow_gnn.sampler import sampling_utils as utils
+
+PCollection = beam.PCollection
+
+
+class TestBalancedLookupJoin(parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      ("empty", [], [], []),
+      ("left_empty", [("a", 1)], [], []),
+      ("right_empty", [], [("a", 2)], []),
+      ("single_value", [("a", 1)], [("a", 2)], [("a", (1, 2))]),
+      ("no_match", [("a", 1)], [("b", 2)], []),
+      ("inner_join", [(2, "y"), (3, "z")], [(1, "a"),
+                                            (2, "b")], [(2, ("y", "b"))]),
+      ("multiple", [(1, "x"), (2, "y")], [(1, "a"),
+                                          (2, "b")], [(1, ("x", "a")),
+                                                      (2, ("y", "b"))]),
+      ("composite_keys", [((1, "x"), "Q")], [((1, "x"), "W")], [((1, "x"),
+                                                                 ("Q", "W"))]),
+      ("composite_values", [(1, ["x", "Q"])], [(1, ["x", "W"])], [
+          (1, (["x", "Q"], ["x", "W"]))
+      ]),
+  )
+  def test_logic(self, queries, values, expected_result):
+    with beam.Pipeline() as root:
+      queries = root | "Queries" >> beam.Create(queries)
+      values = root | "Values" >> beam.Create(values)
+      actual_result = utils.balanced_inner_lookup_join("test", queries, values)
+
+      util.assert_that(actual_result, util.equal_to(expected_result))
+
+  @parameterized.parameters(1, 2, 3, 5, 10, 1000)
+  def test_sharding(self, num_shards: int):
+    with beam.Pipeline() as root:
+      n_samples = 100
+      queries = [(i % 7, i) for i in range(n_samples)]
+      queries.append((-1, -1))
+      values = [(i, str(i)) for i in range(7)]
+      values.append((-2, str(-2)))
+      queries = root | "Queries" >> beam.Create(queries)
+      values = root | "Values" >> beam.Create(values)
+      actual_result = utils.balanced_inner_lookup_join(
+          "test", queries, values, num_shards=num_shards)
+      expected_result = [(i % 7, (i, str(i % 7))) for i in range(n_samples)]
+      util.assert_that(actual_result, util.equal_to(expected_result))
+
+
+class TestUniqueValuesCombiner(parameterized.TestCase):
+
+  def _matcher(self, expected):
+
+    def _sorted(values):
+      result = [(k, sorted(v)) for k, v in values]
+      return sorted(result, key=lambda item: item[0])
+
+    sorted_expected = _sorted(expected)
+
+    def _equal(actual):
+      sorted_actual = _sorted(actual)
+      if sorted_expected != sorted_actual:
+        raise util.BeamAssertException("Failed assert: %r == %r" %
+                                       (sorted_expected, sorted_actual))
+
+    return _equal
+
+  @parameterized.named_parameters(
+      ("empty", [], []),
+      ("distinct1", [(1, ["1"])], [(1, ["1"])]),
+      ("distinct2", [(1, ["1"]), (2, ["1", "2"])], [(1, "1"), (2, ["1", "2"])]),
+      ("equal", [(1, ["1"]), (1, ["1"])], [(1, ["1"])]),
+      ("merging", [("1", [1]), ("2", [1]), ("1", [1, 2]), ("2", [2]),
+                   ("1", [2, 3]), ("2", [1])], [("1", [1, 2, 3]),
+                                                ("2", [1, 2])]),
+  )
+  def test_logic(self, values, expected_result):
+
+    with beam.Pipeline() as root:
+      values = root | "Values" >> beam.Create(values)
+      actual_result = values | "Combine" >> beam.CombinePerKey(
+          utils.unique_values_combiner)
+
+      util.assert_that(actual_result, self._matcher(expected_result))
+
+  @parameterized.parameters(5, 10, 100)
+  def test_max_result_size_satisfied(self, max_result_size: int):
+    input_values = [("x", [i % 5]) for i in range(100)] + [("y", [1])]
+
+    with beam.Pipeline() as root:
+      values = root | "Values" >> beam.Create(input_values)
+      actual_result = values | "Combine" >> beam.CombinePerKey(
+          utils.unique_values_combiner, max_result_size=max_result_size)
+
+      util.assert_that(actual_result,
+                       self._matcher([("x", [0, 1, 2, 3, 4])] + [("y", [1])]))
+
+  @parameterized.parameters(0, 1, 2, 5)
+  def test_max_result_size_violated(self, max_result_size: int):
+    input_values = [("x", [i]) for i in range(6)] + [("y", [1])]
+
+    def run():
+      with beam.Pipeline() as root:
+        values = root | "Values" >> beam.Create(input_values)
+        _ = values | "Combine" >> beam.CombinePerKey(
+            utils.unique_values_combiner, max_result_size=max_result_size)
+
+    self.assertRaisesRegex(ValueError, f"larger than {max_result_size}", run)
+
+
+if __name__ == "__main__":
+  absltest.main()
