d63dbaa65e229ec1adb7f80255572022353c44cd
aferludin <aferludin@google.com>
2022-06-16 09:39:05 -0700

    Allows to encode graph tensor as tf.train.Example using its pieces.
    PiperOrigin-RevId: 455403183

--- tensorflow_gnn/sampler/subgraph.py
+++ tensorflow_gnn/sampler/subgraph.py
@@ -7,18 +7,22 @@ TensorFlow model.
 """
 import collections
 
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Iterable, Mapping, Optional, Tuple, Union
 
 import tensorflow as tf
-import tensorflow_gnn as gnn
+import tensorflow_gnn as tfgnn
 from tensorflow_gnn.sampler import subgraph_pb2
 
-Subgraph = subgraph_pb2.Subgraph
 Example = tf.train.Example
 Feature = tf.train.Feature
+Features = tf.train.Features
+Node = subgraph_pb2.Node
+NodeId = bytes
+SampleId = bytes
+Subgraph = subgraph_pb2.Subgraph
 
 
-def encode_subgraph_to_example(schema: gnn.GraphSchema,
+def encode_subgraph_to_example(schema: tfgnn.GraphSchema,
                                subgraph: Subgraph) -> Example:
   """Convert a Subgraph to an encoded graph tensor."""
 
@@ -43,8 +47,9 @@ def encode_subgraph_to_example(schema: gnn.GraphSchema,
         eset_name, eset_obj, "edges", example)
 
   # Prepare to store edge indices.
-  by_node_set_name: Dict[
-      gnn.NodeSetName, List[subgraph_pb2.Node]] = collections.defaultdict(list)
+  by_node_set_name: Dict[tfgnn.NodeSetName,
+                         List[subgraph_pb2.Node]] = collections.defaultdict(
+                             list)
   for node in subgraph.nodes:
     by_node_set_name[node.node_set_name].append(node)
   index_map: Dict[bytes, int] = {}
@@ -69,8 +74,8 @@ def encode_subgraph_to_example(schema: gnn.GraphSchema,
         raise ValueError("Edge to node outside subgraph: '{}': {}".format(
             edge.neighbor_id, subgraph))
 
-      source_name = "edges/{}.{}".format(edge.edge_set_name, gnn.SOURCE_NAME)
-      target_name = "edges/{}.{}".format(edge.edge_set_name, gnn.TARGET_NAME)
+      source_name = "edges/{}.{}".format(edge.edge_set_name, tfgnn.SOURCE_NAME)
+      target_name = "edges/{}.{}".format(edge.edge_set_name, tfgnn.TARGET_NAME)
       example.features.feature[source_name].int64_list.value.append(source_idx)
       example.features.feature[target_name].int64_list.value.append(target_idx)
       edge_counter[edge.edge_set_name] += 1
@@ -81,10 +86,10 @@ def encode_subgraph_to_example(schema: gnn.GraphSchema,
 
   # Produce size features.
   for node_set_name, num_nodes in node_counter.items():
-    node_size_name = "nodes/{}.{}".format(node_set_name, gnn.SIZE_NAME)
+    node_size_name = "nodes/{}.{}".format(node_set_name, tfgnn.SIZE_NAME)
     example.features.feature[node_size_name].int64_list.value.append(num_nodes)
   for edge_set_name, num_edges in edge_counter.items():
-    edge_size_name = "edges/{}.{}".format(edge_set_name, gnn.SIZE_NAME)
+    edge_size_name = "edges/{}.{}".format(edge_set_name, tfgnn.SIZE_NAME)
     example.features.feature[edge_size_name].int64_list.value.append(num_edges)
 
   # Check the feature sizes (in aggregate).
@@ -102,6 +107,120 @@ def encode_subgraph_to_example(schema: gnn.GraphSchema,
 
   strip_empty_features(example)
   return example
+
+
+def encode_subgraph_pieces_to_example(
+    schema: tfgnn.GraphSchema,
+    seeds: Mapping[tfgnn.NodeSetName, Iterable[NodeId]],
+    context: Features,
+    node_sets: Mapping[tfgnn.NodeSetName, Mapping[NodeId, Features]],
+    edge_sets: Mapping[tfgnn.EdgeSetName, Iterable[Node]]) -> Example:
+  """Convert a subgraph pieces to an encoded graph tensor.
+
+  For details on the serialization format, see the "Data Preparation and
+  Sampling" guide.
+
+  Extra features that are not present in the graph schema are ignored. It is
+  required that all features defined by the graph schema are present in the
+  input.
+
+  Nodes from `seeds` are always added first. The output format allows multiple
+  seed nodes from different node sets. Seed and not seed nodes are sorted by
+  their node ids.
+
+  Edges without matching nodes are filtered out. Edges are deduplicated and
+  sorted using `(source node index, target node index, edge_index)` key.
+
+  Args:
+    schema: The graph schema.
+    seeds: A mapping from a seed node set name to the list of seed node ids.
+    context: Context features.
+    node_sets: A mapping from a node set name to node set features.
+    edge_sets: A mapping from an edge set name to edge set features.
+
+  Returns:
+    Graph tensor encoded as Tensorflow example.
+
+  Raises:
+    ValueError: if some features defined in the `schema` are not present.
+  """
+  result = Example()
+  output_features = result.features.feature
+
+  def add_features(feature_name_prefix: str, features: Features,
+                   features_spec: Mapping[str, Any]) -> None:
+    for fname in features_spec:
+      fvalue = features.feature.get(fname, None)
+      if fvalue is None:
+        raise ValueError(f"Feature '{fname}' is missing from input {features}.")
+
+      output_features[f"{feature_name_prefix}{fname}"].MergeFrom(fvalue)
+
+  def add_size(feature_name_prefix: str, size: int) -> None:
+    assert size > 0, "size=0 cannot not be added."
+    fname = f"{feature_name_prefix}{tfgnn.SIZE_NAME}"
+    output_features[fname].int64_list.value.append(size)
+
+  add_features(f"{tfgnn.CONTEXT}/", context, schema.context.features)
+
+  node_set_to_index = collections.defaultdict(dict)
+  for node_set_name, node_set_spec in schema.node_sets.items():
+    if _is_latent_node_set(schema, node_set_name):
+      nodes = _create_empty_node_features(schema, node_set_name, edge_sets)
+    else:
+      nodes = list(node_sets.get(node_set_name, {}).items())
+
+    if not nodes:
+      continue
+
+    nodes.sort(key=_create_nodes_sort_key(seeds.get(node_set_name, ())))
+
+    feature_name_prefix = f"{tfgnn.NODES}/{node_set_name}."
+    node_indices = node_set_to_index[node_set_name]
+    for node_index, (node_id, features) in enumerate(nodes):
+      add_features(feature_name_prefix, features, node_set_spec.features)
+      node_indices[node_id] = node_index
+    add_size(feature_name_prefix, len(nodes))
+
+  for edge_set_name, edge_set_spec in schema.edge_sets.items():
+    edges: Optional[Iterable[Node]] = edge_sets.get(edge_set_name, None)
+    if edges is None:
+      continue
+
+    source_indexer = node_set_to_index[edge_set_spec.source]
+    target_indexer = node_set_to_index[edge_set_spec.target]
+    filtered_edges = []
+    unique_edge_ids = set()
+    for node in edges:
+      source_index = source_indexer.get(node.id, None)
+      if source_index is None:
+        continue
+
+      for edge in node.outgoing_edges:
+        target_index = target_indexer.get(edge.neighbor_id, None)
+        if target_index is None:
+          continue
+
+        unique_edge_id = (source_index, target_index, edge.edge_index)
+        if unique_edge_id in unique_edge_ids:
+          continue
+        unique_edge_ids.add(unique_edge_id)
+
+        filtered_edges.append((unique_edge_id, edge.features))
+    if not filtered_edges:
+      continue
+
+    filtered_edges.sort(key=_create_edges_sort_key())
+    feature_name_prefix = f"{tfgnn.EDGES}/{edge_set_name}."
+    source_idx = output_features[f"{feature_name_prefix}{tfgnn.SOURCE_NAME}"]
+    target_idx = output_features[f"{feature_name_prefix}{tfgnn.TARGET_NAME}"]
+    for (source_index, target_index, _), features in filtered_edges:
+      add_features(feature_name_prefix, features, edge_set_spec.features)
+      source_idx.int64_list.value.append(source_index)
+      target_idx.int64_list.value.append(target_index)
+    add_size(feature_name_prefix, len(filtered_edges))
+
+  return result
 
 
 _FEATURE_FIELDS = frozenset(["float_list", "int64_list", "bytes_list"])
@@ -157,3 +276,52 @@ def _copy_features(sg_features: tf.train.Feature,
       raise ValueError("Feature '{}' is missing from input: {}".format(
           feature_name, sg_features))
     ex_feature.MergeFrom(sg_feature)
+
+
+def _create_nodes_sort_key(seeds: Iterable[NodeId]):
+  """Creates a sort key that puts seed nodes first and then sorts them by id."""
+
+  seeds = set(seeds)
+
+  def key(item: Tuple[NodeId, Any]) -> Tuple[bool, NodeId]:
+    node_id: NodeId = item[0]
+    return (node_id not in seeds, node_id)
+
+  return key
+
+
+def _create_edges_sort_key():
+  """Create a sort key that puts seed nodes first and then sorts them by id."""
+
+  def key(item: Tuple[Tuple[int, int, int], Any]) -> Tuple[int, int, int]:
+    source_id, target_id, edge_index = item[0]
+    return (source_id, target_id, edge_index)
+
+  return key
+
+
+def _is_latent_node_set(schema: tfgnn.GraphSchema,
+                        node_set_name: tfgnn.NodeSetName) -> bool:
+  """Return `True` if the node set has not features (s.c. latent)."""
+  return not schema.node_sets[node_set_name].features
+
+
+def _create_empty_node_features(
+    schema: tfgnn.GraphSchema, node_set_name: tfgnn.NodeSetName,
+    edges: Mapping[tfgnn.EdgeSetName, Iterable[Node]]
+) -> List[Tuple[NodeId, Features]]:
+  """Create empty features for all latent nodes referenced by edges."""
+  unique_node_ids = set()
+  for edge_set_name, edge_set in schema.edge_sets.items():
+    edges = edges.get(edge_set_name, None)
+    if not edges:
+      continue
+    if edge_set.source == node_set_name:
+      for node in edges:
+        unique_node_ids.add(node.id)
+    if edge_set.target == node_set_name:
+      for node in edges:
+        unique_node_ids.update(
+            [edge.neighbor_id for edge in node.outgoing_edges])
+  dummy_example = Features()
+  return [(node_id, dummy_example) for node_id in unique_node_ids]
--- tensorflow_gnn/sampler/subgraph_test.py
+++ tensorflow_gnn/sampler/subgraph_test.py
@@ -1,8 +1,12 @@
 import copy
 import random
 
+from typing import Mapping, List
+
+from absl.testing import parameterized
+
 import tensorflow as tf
-import tensorflow_gnn as gnn
+import tensorflow_gnn as tfgnn
 from tensorflow_gnn.sampler import subgraph
 from tensorflow_gnn.sampler import subgraph_pb2
 
@@ -69,7 +73,7 @@ class TestSubgraph(tf.test.TestCase):
         """
       context {
         features {
-          key: "stype" value {dtype: DT_STRING}
+          key: "stype" value { dtype: DT_STRING }
         }
       }
       node_sets {
@@ -104,7 +108,7 @@ class TestSubgraph(tf.test.TestCase):
           }
         }
       }
-    """, gnn.GraphSchema())
+    """, tfgnn.GraphSchema())
 
     self.expected = text_format.Parse(
         """
@@ -228,6 +232,301 @@ class TestSubgraph(tf.test.TestCase):
     edge.features.feature["weight"].float_list.value.append(42.)
     with self.assertRaises(ValueError):
       subgraph.encode_subgraph_to_example(self.schema, subgraph_copy)
+
+
+class TestSubgraphPiecesToExample(tf.test.TestCase, parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="empty",
+          schema="",
+          seeds={},
+          context="",
+          node_sets={},
+          edge_sets={},
+          expected_result=""),
+      dict(
+          testcase_name="context_only",
+          schema="""
+            context {
+              features { key: "stype" value { dtype: DT_STRING } }
+              features {
+                key: "itype"
+                value {
+                  dtype: DT_INT64
+                  shape { dim { size: 2 } }
+                }
+              }
+            }
+          """,
+          seeds={},
+          context="""
+            feature { key: "stype" value { bytes_list: { value: ["X"] } } }
+            feature { key: "itype" value { int64_list: { value: [1, 2] } } }
+          """,
+          node_sets={},
+          edge_sets={},
+          expected_result="""
+            features {
+              feature {
+                key: "context/stype" value { bytes_list: { value: ["X"] } }
+              }
+              feature {
+                key: "context/itype" value { int64_list: { value: [1, 2] } }
+              }
+            }
+          """),
+      dict(
+          testcase_name="nodes_only",
+          schema="""
+            node_sets {
+              key: "A"
+              value {
+                features { key: "stype" value { dtype: DT_STRING } }
+              }
+            }
+            node_sets {
+              key: "B"
+              value {
+                features { key: "ftype" value {dtype: DT_FLOAT} }
+              }
+            }
+          """,
+          seeds={"A": [b"a2"]},
+          context="",
+          node_sets={
+              "A": {
+                  b"a1": """
+                    feature {
+                      key: "stype"
+                      value { bytes_list: { value: ["1"] } }
+                    }
+                  """,
+                  b"a2": """
+                    feature {
+                      key: "stype"
+                      value { bytes_list: { value: ["2"] } }
+                    }
+                  """,
+                  b"a3": """
+                    feature {
+                      key: "stype"
+                      value { bytes_list: { value: ["3"] } }
+                    }
+                  """
+              },
+              "B": {
+                  b"b1": """
+                    feature {
+                      key: "ftype"
+                      value { float_list: { value: [1.0] } }
+                    }
+                  """,
+                  b"b2": """
+                    feature {
+                      key: "ftype"
+                      value { float_list: { value: [2.0] } }
+                    }
+                  """
+              },
+          },
+          edge_sets={},
+          expected_result="""
+            features {
+              feature {
+                key: "nodes/A.stype"
+                value { bytes_list: { value: ["2", "1", "3"] } }
+              }
+              feature {
+                key: "nodes/A.#size"
+                value { int64_list: { value: [3] } }
+              }
+              feature {
+                key: "nodes/B.ftype"
+                value { float_list: { value: [1.0, 2.0] } }
+              }
+              feature {
+                key: "nodes/B.#size"
+                value { int64_list: { value: [2] } }
+              }
+            }
+          """),
+      dict(
+          testcase_name="edges_only",
+          schema="""
+            node_sets {
+              key: "A"
+              value {}
+            }
+            node_sets {
+              key: "B"
+              value {}
+            }
+            edge_sets {
+              key: "A->B"
+              value {
+                source: "A"
+                target: "B"
+                features { key: "i" value {dtype: DT_INT64} }
+              }
+            }
+          """,
+          seeds={},
+          context="",
+          node_sets={},
+          edge_sets={
+              "A->B": [
+                  """
+                    id: "a2"
+                    outgoing_edges {
+                      neighbor_id: "b3"
+                      features {
+                        feature { key: "i" value { int64_list { value: 23 } } }
+                      }
+                    }
+                    outgoing_edges {
+                      neighbor_id: "b1"
+                      features {
+                        feature { key: "i" value { int64_list { value: 21 } } }
+                      }
+                    }
+                  """,
+                  """
+                    id: "a1"
+                    outgoing_edges {
+                      neighbor_id: "b2"
+                      features {
+                        feature { key: "i" value { int64_list { value: 12 } } }
+                      }
+                    }
+                  """,
+              ]
+          },
+          expected_result="""
+            features {
+              feature {
+                key: "nodes/A.#size" value { int64_list: { value: [2] } }
+              }
+              feature {
+                key: "nodes/B.#size" value { int64_list: { value: [3] } }
+              }
+              feature {
+                key: "edges/A->B.#size" value { int64_list: { value: [3] } }
+              }
+              feature {
+                key: "edges/A->B.#source"
+                value { int64_list: { value: [0, 1, 1] } }
+              }
+              feature {
+                key: "edges/A->B.#target"
+                value { int64_list: { value: [1, 0, 2] } }
+              }
+              feature {
+                key: "edges/A->B.i"
+                value {int64_list: { value: [12, 21, 23] } }
+              }
+            }
+          """),
+      dict(
+          testcase_name="homogeneous",
+          schema="""
+            node_sets {
+              key: "node"
+              value {
+                features { key: "id" value { dtype: DT_STRING } }
+              }
+            }
+            edge_sets {
+              key: "edge"
+              value {
+                source: "node"
+                target: "node"
+              }
+            }
+          """,
+          seeds={},
+          context="",
+          node_sets={
+              "node": {
+                  b"3": """
+                    feature {
+                      key: "id"
+                      value { bytes_list: { value: ["3"] } }
+                    }
+                  """,
+                  b"2": """
+                    feature {
+                      key: "id"
+                      value { bytes_list: { value: ["2"] } }
+                    }
+                  """,
+                  b"1": """
+                    feature {
+                      key: "id"
+                      value { bytes_list: { value: ["1"] } }
+                    }
+                  """
+              },
+          },
+          edge_sets={
+              "edge": [
+                  """
+                    id: "3"
+                    outgoing_edges { neighbor_id: "1" }
+                    outgoing_edges { neighbor_id: "2" }
+                  """,
+                  """
+                    id: "1"
+                    outgoing_edges { neighbor_id: "3" }
+                    outgoing_edges { neighbor_id: "1" }
+                  """,
+                  """
+                    id: "2"
+                    outgoing_edges { neighbor_id: "1" }
+                    outgoing_edges { neighbor_id: "3" }
+                  """,
+              ]
+          },
+          expected_result="""
+            features {
+              feature {
+                key: "nodes/node.#size" value { int64_list: { value: [3] } }
+              }
+              feature {
+                key: "nodes/node.id"
+                value { bytes_list: { value: ["1", "2", "3"] } }
+              }
+              feature {
+                key: "edges/edge.#size" value { int64_list: { value: [6] } }
+              }
+              feature {
+                key: "edges/edge.#source"
+                value { int64_list: { value: [0, 0, 1, 1, 2, 2] } }
+              }
+              feature {
+                key: "edges/edge.#target"
+                value { int64_list: { value: [0, 2, 0, 2, 0, 1] } }
+              }
+            }
+          """))
+  def test_logic(self, schema: str,
+                 seeds: Mapping[tfgnn.NodeSetName, List[bytes]],
+                 context: str,
+                 node_sets: Mapping[tfgnn.NodeSet, Mapping[bytes, str]],
+                 edge_sets: Mapping[tfgnn.NodeSet, List[str]],
+                 expected_result: str):
+    schema = text_format.Parse(schema, tfgnn.GraphSchema())
+    context = text_format.Parse(context, tf.train.Features())
+    node_sets = tf.nest.map_structure(
+        lambda v: text_format.Parse(v, tf.train.Features()), node_sets)
+    edge_sets = tf.nest.map_structure(
+        lambda v: text_format.Parse(v, subgraph_pb2.Node()), edge_sets)
+
+    actual_result = subgraph.encode_subgraph_pieces_to_example(
+        schema, seeds, context, node_sets, edge_sets)
+
+    expected_result = text_format.Parse(expected_result, tf.train.Example())
+    self.assertProtoEquals(actual_result, expected_result)
 
 
 if __name__ == "__main__":
