3901a4f0fd45cc61965f14bb08824c39f0403c65
aferludin <aferludin@google.com>
2022-06-17 08:49:00 -0700

    Implements edge sampling for all supported sampling strategies.
    PiperOrigin-RevId: 455627531

---
+++ tensorflow_gnn/sampler/sampling_lib.py
@@ -0,0 +1,290 @@
+"""Collection of graph sampling algorithms."""
+
+import collections
+import math
+import random
+from typing import Callable, DefaultDict, Dict, List, Tuple
+
+import apache_beam as beam
+import tensorflow as tf
+import tensorflow_gnn as tfgnn
+from tensorflow_gnn.sampler import sampling_spec_pb2
+from tensorflow_gnn.sampler import sampling_utils as utils
+from tensorflow_gnn.sampler import subgraph_pb2
+
+PCollection = beam.pvalue.PCollection
+NodeId = bytes
+SampleId = bytes
+Example = tf.train.Example
+Features = tf.train.Features
+Node = subgraph_pb2.Node
+Edge = subgraph_pb2.Node.Edge
+SampledEdges = PCollection[Tuple[SampleId, Node]]
+
+WeightFunc = Callable[[Edge], float]
+
+UniqueNodeIds = PCollection[Tuple[SampleId, List[NodeId]]]
+
+inner_lookup_join = utils.balanced_inner_lookup_join
+
+
+def get_weight_feature(edge: Edge, default_value: float = 1.0) -> float:
+  """Return the weight feature or a default value (1.), if not present."""
+  feature = edge.features.feature.get("weight", None)
+  if feature is None:
+    return default_value
+  value = feature.float_list.value
+  return value[0] if value else default_value
+
+
+def create_sampling_weight_fn(sampling_op: sampling_spec_pb2.SamplingOp,
+                              weight_fn: WeightFunc) -> WeightFunc:
+  """Returns weight function that should be used for edge sampling.
+
+  This function should be used to create weights for reservoir sampling. E.g.
+  to sample K instances from a stream of edges according to the `sampling_op`
+  one needs to extract top-k examples according to this funciton.
+
+  For non-determinsitic sampling algorithms (like RANDOM_UNIFORM) this function
+  could return different values for the same input.
+
+  Args:
+    sampling_op: The sampling operation.
+    weight_fn: The function that reads the weight value of an edge.
+
+  Returns:
+    An edge weight function that should be used for reservoir edge sampling.
+
+  Raises:
+    ValueError: if sampling operation is not supported.
+  """
+  if sampling_op.strategy == sampling_spec_pb2.TOP_K:
+    return weight_fn
+
+  if sampling_op.strategy == sampling_spec_pb2.RANDOM_UNIFORM:
+    return lambda _: random.uniform(0., 1.)
+
+  if sampling_op.strategy == sampling_spec_pb2.RANDOM_WEIGHTED:
+
+    def random_weighted_fn(edge: Edge) -> float:
+      noise = random.uniform(0.0, 1.0)
+      weight = weight_fn(edge)
+      if weight <= 0.0 or noise <= 0.0:
+        return float("-inf")
+      return math.log(noise) / weight
+
+    return random_weighted_fn
+
+  raise ValueError(f"Unsupported samplign strategy f{sampling_op.strategy}.")
+
+
+_DETERMINISTIC_SAMPLING_STRATEGIES = {
+    sampling_spec_pb2.TOP_K: True,
+    sampling_spec_pb2.RANDOM_UNIFORM: False,
+    sampling_spec_pb2.RANDOM_WEIGHTED: False
+}
+
+
+def is_deterministic(sampling_op: sampling_spec_pb2.SamplingOp) -> bool:
+  """Returns `True` if sampling operation produces resample_for_each_path results."""
+  return _DETERMINISTIC_SAMPLING_STRATEGIES.get(sampling_op.strategy)
+
+
+# Sampling frontier is a mapping from (sample id, node id) pair to the number
+# of sampling paths that lead to that node id within the same subgraph.
+Frontier = PCollection[Tuple[Tuple[SampleId, NodeId], int]]
+
+_FROTIER_OUTPUT_TAG = "frontier"
+
+
+class ResevoirEdgeSamplingFn(beam.DoFn):
+  """Implements reservoir sampling without replacement.
+
+  Sampling is done by accumulating top `sample_size` edges according to the
+  weight function `weight_fn`.
+  """
+
+  def __init__(self, weight_fn: WeightFunc, sample_size: int,
+               resample_for_each_path: bool):
+    """Constructor.
+
+    Args:
+      weight_fn: A function that takes an edge as its input and returns weight
+        to use for reservoir sampling as its output. Could be non-deterministic.
+      sample_size: The upper bound on the number of sampled edges for each input
+        node or (node, path) pair (see `resample_for_each_path`).
+      resample_for_each_path: If `False`, the sampling is done once for each
+        input node. If `True`, the sampling is repeated number of paths times.
+    """
+    self._weight_fn = weight_fn
+    self._sample_size = sample_size
+    self._resample_for_each_path = resample_for_each_path
+
+  def process(self, element: Tuple[SampleId, Tuple[int, Node]]):
+    """Samples edges for each input and computes new sampling frontier.
+
+    Args:
+      element: A tuple containing a sample id mapping to a tuple containing
+      an integer count of the total number of sampling paths that lead to a
+      given node.
+
+    Yields:
+      Tuple of sample id and sampled edged as the main output and new sampling
+        frontier as `_FROTIER_OUTPUT_TAG` output.
+    """
+    sample_id, (num_samples, node) = element
+
+    if len(node.outgoing_edges) <= self._sample_size:
+      # Optimization: because sampling is done without replacement we could
+      # return all edges if the sample size if greater than the total number of
+      # outgoing edges.
+      sampled_edges = node
+      for edge in node.outgoing_edges:
+        yield beam.pvalue.TaggedOutput(
+            _FROTIER_OUTPUT_TAG, ((sample_id, edge.neighbor_id), num_samples))
+
+    else:
+      # Map from sampled edge indices (in `node.outgoing_edges`) to the total
+      # number of times this edge was sampled for all sampling paths.
+      edge_index_to_count = collections.defaultdict(int)
+      if self._resample_for_each_path:
+        num_resamples, count_step = num_samples, 1
+      else:
+        num_resamples, count_step = 1, num_samples
+      for _ in range(num_resamples):
+        weights: List[Tuple[float, int]] = []
+        for edge_index, edge in enumerate(node.outgoing_edges):
+          weights.append((self._weight_fn(edge), edge_index))
+        weights.sort(reverse=True)
+        for _, edge_index in weights[:self._sample_size]:
+          edge_index_to_count[edge_index] += count_step
+
+      sampled_edges = Node()
+      sampled_edges.CopyFrom(node)
+      sampled_edges.ClearField("outgoing_edges")
+      sampled_edges.outgoing_edges.extend(
+          [node.outgoing_edges[index] for index in edge_index_to_count.keys()])
+
+      for edge_index, count in edge_index_to_count.items():
+        edge = node.outgoing_edges[edge_index]
+        yield beam.pvalue.TaggedOutput(_FROTIER_OUTPUT_TAG,
+                                       ((sample_id, edge.neighbor_id), count))
+
+    yield sample_id, sampled_edges
+
+
+def sample_edges(
+    sampling_spec: sampling_spec_pb2.SamplingSpec,
+    seeds: Dict[tfgnn.NodeSetName, PCollection[Tuple[SampleId, NodeId]]],
+    adj_lists: Dict[tfgnn.EdgeSetName, PCollection[Node]]
+) -> Dict[tfgnn.EdgeSetName, SampledEdges]:
+  """Samples edges from the graph according to the sampling specification.
+
+  Sampling starts from the seed nodes provided in the `seeds` collection. Each
+  edge sampling operation first aggregates nodes produced by all its input
+  operations. Then it samples from nodes' outgoing edges independently for
+  each node.
+
+  Args:
+    sampling_spec: The specification that defines how we will perform sampling.
+    seeds: A mapping from node set name to (sample id, seed node id) pairs.
+    adj_lists: A mapping from edge set names to adjacency lists. An adjacency
+      list is a pair of a source node id and all outgoing edges for that source
+      node (as Node message). Currently it is assumed that adjacency lists are
+      "small" (much smaller than a single worker memory).
+
+  Returns:
+    A mapping from the edge set name to all sampled edges. Note that the
+    function does not perform sampled edges deduplication so the same edge could
+    appear multiple times for the same sample id.
+
+  Raises:
+    ValueError: If sampling specification is not correct.
+  """
+
+  def key_by_id(node: Node) -> Tuple[NodeId, Node]:
+    return node.id, node
+
+  adj_lists = {
+      set_name: nodes | f"KeyByNodeId/{set_name}" >> beam.Map(key_by_id)
+      for set_name, nodes in adj_lists.items()
+  }
+  op_to_frontier: Dict[str, Frontier] = {}
+  for seed_op in [sampling_spec.seed_op]:
+    node_set_name: tfgnn.NodeSetName = seed_op.node_set_name
+    if seed_op.node_set_name not in seeds:
+      raise ValueError(
+          f"No seeds provided for '{seed_op.node_set_name}' from {seed_op}.")
+
+    op_to_frontier[seed_op.op_name] = (
+        seeds[node_set_name]
+        | f"CreateFrontier/{node_set_name}" >> beam.Map(lambda item: (item, 1)))
+
+  edge_set_to_sampled_edges: DefaultDict[
+      tfgnn.EdgeSetName, List[SampledEdges]] = collections.defaultdict(list)
+
+  def execute_op(sampling_op: sampling_spec_pb2.SamplingOp) -> None:
+    if sampling_op.edge_set_name not in adj_lists:
+      raise ValueError((
+          f"Edge set '{sampling_op.edge_set_name}' from the"
+          f" '{sampling_op.op_name}' sampling op "
+          " is not defined in the graph schema."))
+
+    stage_name = lambda suffix: f"{sampling_op.op_name}/{suffix}"
+
+    op_inputs = []
+    for input_op_name in sampling_op.input_op_names:
+      frontier_piece = op_to_frontier.get(input_op_name, None)
+      if frontier_piece is None:
+        raise ValueError(f"Could not match '{input_op_name}' input from"
+                         f" the '{sampling_op.op_name}' sampling operation."
+                         " Note that it is currently assumed that sampling"
+                         " operations in the sampling spec are ordered in the"
+                         " order of their possible execution.")
+      op_inputs.append(frontier_piece)
+
+    frontier: Frontier = (
+        op_inputs
+        | stage_name("FlattenInputs") >> beam.Flatten()
+        | stage_name("GroupSamplingPaths") >> beam.CombinePerKey(sum))
+
+    def extract_nodes(query: Tuple[SampleId, int],
+                      value: Node) -> Tuple[SampleId, Tuple[int, Node]]:
+      sample_id, num_paths = query
+      return sample_id, (num_paths, value)
+
+    def rekey_by_source_id(
+        item: Tuple[SampleId, NodeId],
+        num_paths: int) -> Tuple[NodeId, Tuple[SampleId, int]]:
+      return item[1], (item[0], num_paths)
+
+    nodes = (
+        inner_lookup_join(
+            stage_name("LookupNodes"),
+            queries=(frontier
+                     | stage_name("RekeyFrontierBySourceId") >>
+                     beam.MapTuple(rekey_by_source_id)),
+            lookup_table=adj_lists[sampling_op.edge_set_name])
+        | stage_name("DropSourceIds") >> beam.Values()
+        | stage_name("ExtractMatchedNodes") >> beam.MapTuple(extract_nodes))
+
+    sampling_fn = ResevoirEdgeSamplingFn(
+        create_sampling_weight_fn(sampling_op, get_weight_feature),
+        sampling_op.sample_size,
+        resample_for_each_path=not is_deterministic(sampling_op))
+    sampled_edges, new_frontier = (
+        nodes
+        | stage_name("SampleEdges") >> beam.ParDo(sampling_fn).with_outputs(
+            _FROTIER_OUTPUT_TAG, main="sampled_edges"))
+
+    op_to_frontier[sampling_op.op_name] = new_frontier
+    edge_set_to_sampled_edges[sampling_op.edge_set_name].append(sampled_edges)
+
+  for sampling_op in sampling_spec.sampling_ops:
+    execute_op(sampling_op)
+
+  return {
+      edge_set_name:
+      (result_pieces | f"FlattenSampledEdges/{edge_set_name}" >> beam.Flatten())
+      for edge_set_name, result_pieces in edge_set_to_sampled_edges.items()
+  }
---
+++ tensorflow_gnn/sampler/sampling_lib_test.py
@@ -0,0 +1,436 @@
+"""Tests for sampling_lib."""
+
+import math
+from typing import Iterable, List, Mapping, Tuple
+
+from absl.testing import parameterized
+import apache_beam as beam
+from apache_beam.testing import util
+import tensorflow as tf
+import tensorflow_gnn as tfgnn
+from tensorflow_gnn.sampler import sampling_lib as lib
+from tensorflow_gnn.sampler import sampling_spec_pb2
+from tensorflow_gnn.sampler import subgraph_pb2
+
+from google.protobuf import text_format
+
+PCollection = beam.PCollection
+SamplingOp = sampling_spec_pb2.SamplingOp
+SamplingSpec = sampling_spec_pb2.SamplingSpec
+SamplingStrategy = sampling_spec_pb2.SamplingStrategy
+
+Edge = subgraph_pb2.Node.Edge
+Node = subgraph_pb2.Node
+NodeId = lib.NodeId
+SampleId = lib.SampleId
+
+
+def _get_op(strategy: SamplingStrategy, sample_size: int) -> SamplingOp:
+  result = SamplingOp()
+  result.sample_size = sample_size
+  result.strategy = strategy
+  return result
+
+
+def _get_test_edge(weight: float) -> Edge:
+  edge = Edge()
+  edge.features.feature["weight"].float_list.value.append(weight)
+  return edge
+
+
+class TestHelperFunctions(parameterized.TestCase):
+
+  @parameterized.parameters(0.0, 1.0, 2.0)
+  def test_sampling_weight_fn_top_k(self, edge_weight: float):
+    weight_fn = lib.create_sampling_weight_fn(
+        _get_op(SamplingStrategy.TOP_K, 10), lib.get_weight_feature)
+    for _ in range(1_000):
+      edge = _get_test_edge(edge_weight)
+      self.assertEqual(weight_fn(edge), edge_weight)
+
+  @parameterized.parameters(0.0, 1.0, 2.0)
+  def test_sampling_weight_fn_random_uniform(self, edge_weight: float):
+    weight_fn = lib.create_sampling_weight_fn(
+        _get_op(SamplingStrategy.RANDOM_UNIFORM, 10), lib.get_weight_feature)
+
+    weights = [weight_fn(_get_test_edge(edge_weight)) for _ in range(1_000)]
+    hist = set(int(weight * 10.0) for weight in weights)
+    # For the uniform distribution the probability that any histogram has not
+    # values is 0.9^1_000.
+    self.assertCountEqual(hist, range(10))
+
+  def test_sampling_weight_fn_random_weighted(self):
+    weight_fn = lib.create_sampling_weight_fn(
+        _get_op(SamplingStrategy.RANDOM_WEIGHTED, 10), lib.get_weight_feature)
+
+    self.assertLess(weight_fn(_get_test_edge(0.0)), -1.0e10)
+
+    n_samples = 10_000
+    w1 = [(weight_fn(_get_test_edge(1.0)), 1) for i in range(10 * n_samples)]
+    w3 = [(weight_fn(_get_test_edge(3.0)), 3) for i in range(10 * n_samples)]
+
+    w1_count = 0
+    for _, group in sorted(w1 + w3, reverse=True)[:n_samples]:
+      self.assertIn(group, [1, 3])
+      w1_count += int(group == 1)
+
+    ratio = 1.0 * w1_count / n_samples
+    std = math.sqrt(0.25 * 0.75 / n_samples)
+    self.assertLess(ratio, 0.25 + 6.0 * std)
+    self.assertGreater(ratio, 0.25 - 6.0 * std)
+
+  def test_is_deterministic(self):
+    self.assertTrue(lib.is_deterministic(_get_op(SamplingStrategy.TOP_K, 1)))
+    self.assertFalse(
+        lib.is_deterministic(_get_op(SamplingStrategy.RANDOM_UNIFORM, 1)))
+    self.assertFalse(
+        lib.is_deterministic(_get_op(SamplingStrategy.RANDOM_WEIGHTED, 1)))
+
+
+def _create_test_node(node_id: int, neighbor_ids: List[int]) -> Node:
+  node = Node()
+  node.id = str(node_id).encode()
+  for neighbor_id in neighbor_ids:
+    edge: Edge = node.outgoing_edges.add()
+    edge.MergeFrom(_get_test_edge(float(neighbor_id)))
+    edge.neighbor_id = str(neighbor_id).encode()
+  return node
+
+
+def _create_test_nodes(
+    inputs: List[Tuple[SampleId, int,
+                       List[int]]]) -> List[Tuple[SampleId, Node]]:
+  result = []
+  for sample_id, node_index, neighbors in inputs:
+    result.append((sample_id, _create_test_node(node_index, neighbors)))
+  return result
+
+
+class EdgeSamplingTestBase(tf.test.TestCase, parameterized.TestCase):
+
+  def sampled_edges_matcher(self, expected: Iterable[Tuple[SampleId, Node]]):
+
+    def key_fn(item):
+      sample_id, node = item
+      return sample_id, node.id
+
+    sorted_expected = sorted(expected, key=key_fn)
+
+    def _equal(actual):
+      sorted_actual = sorted(actual, key=key_fn)
+
+      msg = f"Failed assert: {sorted_expected} == {sorted_actual}"
+
+      self.assertEqual(len(sorted_expected), len(sorted_actual), msg=msg)
+
+      for l, r in zip(sorted_expected, sorted_actual):
+        l_sample_id, l_node = l
+        r_sample_id, r_node = r
+        self.assertProtoEquals(l_node, r_node, msg=msg)
+        self.assertEqual(l_sample_id, r_sample_id, msg=msg)
+
+    return _equal
+
+
+class TestReservoirEdgeSamplingFn(EdgeSamplingTestBase):
+
+  def _add_num_paths(
+      self, nodes: List[Tuple[SampleId, Node]],
+      num_paths: List[int]) -> List[Tuple[SampleId, Tuple[int, Node]]]:
+    self.assertEqual(len(nodes), len(nodes))
+    return [(sample_id, (count, node))
+            for (sample_id, node), count in zip(nodes, num_paths)]
+
+  @parameterized.parameters([
+      SamplingStrategy.TOP_K, SamplingStrategy.RANDOM_UNIFORM,
+      SamplingStrategy.RANDOM_WEIGHTED
+  ])
+  def test_all(self, strategy: SamplingStrategy):
+    sampling_op = _get_op(strategy, 3)
+    sampling_fn = lib.ResevoirEdgeSamplingFn(
+        lib.create_sampling_weight_fn(sampling_op, lib.get_weight_feature),
+        sampling_op.sample_size,
+        resample_for_each_path=not lib.is_deterministic(sampling_op))
+    with beam.Pipeline() as root:
+      nodes = _create_test_nodes([(b"sample.1", 1, [3, 2, 1]),
+                                  (b"sample.1", 2, [2, 1]),
+                                  (b"sample.2", 3, [2])])
+      sampled_edges, new_frontier = (
+          root | beam.Create(self._add_num_paths(nodes, [10, 20, 30]))
+          | beam.ParDo(sampling_fn).with_outputs(
+              lib._FROTIER_OUTPUT_TAG, main="sampled_edges"))
+      util.assert_that(
+          sampled_edges, self.sampled_edges_matcher(nodes), label="samples")
+      util.assert_that(
+          new_frontier,
+          util.equal_to([
+              ((b"sample.1", b"1"), 10),
+              ((b"sample.1", b"2"), 10),
+              ((b"sample.1", b"3"), 10),
+              ((b"sample.1", b"1"), 20),
+              ((b"sample.1", b"2"), 20),
+              ((b"sample.2", b"2"), 30),
+          ]),
+          label="frontier")
+
+  def test_top_2(self):
+    sampling_op = _get_op(SamplingStrategy.TOP_K, 2)
+    sampling_fn = lib.ResevoirEdgeSamplingFn(
+        lib.create_sampling_weight_fn(sampling_op, lib.get_weight_feature),
+        sampling_op.sample_size,
+        resample_for_each_path=False)
+    with beam.Pipeline() as root:
+      nodes = _create_test_nodes([(b"sample.1", 1, [3, 2, 1]),
+                                  (b"sample.1", 2, [2, 1]),
+                                  (b"sample.2", 3, [2])])
+      sampled_edges, new_frontier = (
+          root | beam.Create(self._add_num_paths(nodes, [10, 20, 30]))
+          | beam.ParDo(sampling_fn).with_outputs(
+              lib._FROTIER_OUTPUT_TAG, main="sampled_edges"))
+      expected_nodes = _create_test_nodes([(b"sample.1", 1, [3, 2]),
+                                           (b"sample.1", 2, [2, 1]),
+                                           (b"sample.2", 3, [2])])
+      util.assert_that(
+          sampled_edges,
+          self.sampled_edges_matcher(expected_nodes),
+          label="samples")
+      util.assert_that(
+          new_frontier,
+          util.equal_to([
+              ((b"sample.1", b"2"), 10),
+              ((b"sample.1", b"3"), 10),
+              ((b"sample.1", b"1"), 20),
+              ((b"sample.1", b"2"), 20),
+              ((b"sample.2", b"2"), 30),
+          ]),
+          label="frontier")
+
+  def test_top_1(self):
+    sampling_op = _get_op(SamplingStrategy.TOP_K, 1)
+    sampling_fn = lib.ResevoirEdgeSamplingFn(
+        lib.create_sampling_weight_fn(sampling_op, lib.get_weight_feature),
+        sampling_op.sample_size,
+        resample_for_each_path=False)
+    with beam.Pipeline() as root:
+      nodes = _create_test_nodes([(b"sample.1", 1, [1, 2, 3, 1]),
+                                  (b"sample.1", 2, [2, 1]),
+                                  (b"sample.2", 3, [2])])
+      sampled_edges, new_frontier = (
+          root | beam.Create(self._add_num_paths(nodes, [1, 1, 1]))
+          | beam.ParDo(sampling_fn).with_outputs(
+              lib._FROTIER_OUTPUT_TAG, main="sampled_edges"))
+      expected_nodes = _create_test_nodes([(b"sample.1", 1, [3]),
+                                           (b"sample.1", 2, [2]),
+                                           (b"sample.2", 3, [2])])
+      util.assert_that(
+          sampled_edges,
+          self.sampled_edges_matcher(expected_nodes),
+          label="samples")
+      util.assert_that(
+          new_frontier,
+          util.equal_to([
+              ((b"sample.1", b"3"), 1),
+              ((b"sample.1", b"2"), 1),
+              ((b"sample.2", b"2"), 1),
+          ]),
+          label="frontier")
+
+  def test_non_deterministic(self):
+    mock_weights = {b"1": [2, 1, 2], b"2": [1, 2, 1], b"3": [0, 0, 0]}
+
+    def mock_weight_fn(edge: Edge) -> float:
+      return mock_weights[edge.neighbor_id].pop(0)
+
+    sampling_op = _get_op(SamplingStrategy.RANDOM_UNIFORM, 1)
+    sampling_fn = lib.ResevoirEdgeSamplingFn(
+        mock_weight_fn, sampling_op.sample_size, resample_for_each_path=True)
+    with beam.Pipeline() as root:
+      nodes = _create_test_nodes([(b"sample.1", 1, [1, 2, 3])])
+      sampled_edges, new_frontier = (
+          root | beam.Create(self._add_num_paths(nodes, [3]))
+          | beam.ParDo(sampling_fn).with_outputs(
+              lib._FROTIER_OUTPUT_TAG, main="sampled_edges"))
+      expected_nodes = _create_test_nodes([(b"sample.1", 1, [1, 2])])
+      util.assert_that(
+          sampled_edges,
+          self.sampled_edges_matcher(expected_nodes),
+          label="samples")
+      util.assert_that(
+          new_frontier,
+          util.equal_to([
+              ((b"sample.1", b"1"), 2),
+              ((b"sample.1", b"2"), 1),
+          ]),
+          label="frontier")
+
+
+class TestEdgeSampling(EdgeSamplingTestBase):
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="empty",
+          sampling_spec="""
+            seed_op {
+              op_name: 'seed'
+              node_set_name: 'node'
+            }
+          """,
+          seeds={"node": []},
+          adj_lists={},
+          expected_result={}),
+      dict(
+          testcase_name="no_sampling",
+          sampling_spec="""
+            seed_op {
+              op_name: 'seed'
+              node_set_name: 'node'
+            }
+          """,
+          seeds={"node": [(b"s.1", b"1")]},
+          adj_lists={"edge": [_create_test_node(1, [1, 2])]},
+          expected_result={}),
+      dict(
+          testcase_name="one_hop",
+          sampling_spec="""
+            seed_op {
+              op_name: 'seed'
+              node_set_name: 'node'
+            }
+            sampling_ops {
+              op_name: 'hop-1'
+              input_op_names: [ 'seed' ]
+              strategy: TOP_K
+              sample_size: 2
+              edge_set_name: 'edge'
+            }
+          """,
+          seeds={"node": [
+              (b"s.1.0", b"1"),
+              (b"s.1.1", b"1"),
+              (b"s.2", b"2"),
+          ]},
+          adj_lists={
+              "edge": [
+                  _create_test_node(1, [1, 2, 3]),
+                  _create_test_node(2, [1])
+              ]
+          },
+          expected_result={
+              "edge": [
+                  (b"s.1.0", _create_test_node(1, [3, 2])),
+                  (b"s.1.1", _create_test_node(1, [3, 2])),
+                  (b"s.2", _create_test_node(2, [1])),
+              ]
+          }),
+      dict(
+          testcase_name="two_hops",
+          sampling_spec="""
+            seed_op {
+              op_name: 'seed'
+              node_set_name: 'node'
+            }
+            sampling_ops {
+              op_name: 'hop-1'
+              input_op_names: [ 'seed' ]
+              strategy: TOP_K
+              sample_size: 2
+              edge_set_name: 'edge'
+            }
+            sampling_ops {
+              op_name: 'hop-2'
+              input_op_names: [ 'hop-1' ]
+              strategy: TOP_K
+              sample_size: 2
+              edge_set_name: 'edge'
+            }
+          """,
+          seeds={"node": [(b"s.1", b"1"),]},
+          adj_lists={
+              "edge": [
+                  _create_test_node(1, [1, 2, 3]),
+                  _create_test_node(2, [1, 4, 3, 2]),
+                  _create_test_node(3, [1]),
+                  _create_test_node(4, [1, 4])
+              ]
+          },
+          expected_result={
+              "edge": [
+                  (b"s.1", _create_test_node(1, [3, 2])),
+                  (b"s.1", _create_test_node(2, [4, 3])),
+                  (b"s.1", _create_test_node(3, [1])),
+              ]
+          }),
+      dict(
+          testcase_name="merging",
+          sampling_spec="""
+            seed_op {
+              op_name: 'seed'
+              node_set_name: 'A'
+            }
+            sampling_ops {
+              op_name: 'A->B:1'
+              input_op_names: [ 'seed' ]
+              strategy: TOP_K
+              sample_size: 2
+              edge_set_name: 'A->B:1'
+            }
+            sampling_ops {
+              op_name: 'A->B:2'
+              input_op_names: [ 'seed' ]
+              strategy: TOP_K
+              sample_size: 2
+              edge_set_name: 'A->B:2'
+            }
+            sampling_ops {
+              op_name: 'B->A'
+              input_op_names: [ 'A->B:1',  'A->B:2']
+              strategy: TOP_K
+              sample_size: 2
+              edge_set_name: 'B->A'
+            }
+          """,
+          seeds={"A": [(b"s.1", b"1"), (b"s.2", b"2")]},
+          adj_lists={
+              "A->B:1": [
+                  _create_test_node(1, [1, 2, 3]),
+                  _create_test_node(2, [1, 4, 5, 2]),
+              ],
+              "A->B:2": [_create_test_node(1, [2]),],
+              "B->A": [_create_test_node(3, [1]),]
+          },
+          expected_result={
+              "A->B:1": [(b"s.1", _create_test_node(1, [3, 2])),
+                         (b"s.2", _create_test_node(2, [5, 4]))],
+              "A->B:2": [(b"s.1", _create_test_node(1, [2])),],
+              "B->A": [(b"s.1", _create_test_node(3, [1])),]
+          }),
+  )
+  def test_logic(self, sampling_spec: str,
+                 seeds: Mapping[tfgnn.NodeSetName, Iterable[Tuple[SampleId,
+                                                                  NodeId]]],
+                 adj_lists: Mapping[tfgnn.EdgeSetName, Iterable[Node]],
+                 expected_result: Mapping[tfgnn.EdgeSetName,
+                                          Iterable[Tuple[SampleId, Node]]]):
+    sampling_spec = text_format.Parse(sampling_spec, SamplingSpec())
+    with beam.Pipeline() as root:
+      seeds = {
+          set_name: root | f"Seeds/{set_name}" >> beam.Create(values)
+          for set_name, values in seeds.items()
+      }
+
+      adj_lists = {
+          set_name: root | f"AdjLists/{set_name}" >> beam.Create(values)
+          for set_name, values in adj_lists.items()
+      }
+
+      actual_result = lib.sample_edges(sampling_spec, seeds, adj_lists)
+      self.assertCountEqual(actual_result.keys(), expected_result.keys())
+      for edge_set_name in expected_result:
+        util.assert_that(
+            actual_result[edge_set_name],
+            self.sampled_edges_matcher(expected_result[edge_set_name]),
+            label=edge_set_name)
+
+
+if __name__ == "__main__":
+  tf.test.main()
