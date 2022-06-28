c02ecf797ce01b6d3b739be05ac753e1fedf82a7
mparadkar <mparadkar@google.com>
2022-06-28 09:49:38 -0700

    Fixes CI build script to use wheel version from main.
    PiperOrigin-RevId: 457755381

--- kokoro/github/ubuntu/cpu/build.sh
+++ kokoro/github/ubuntu/cpu/build.sh
@@ -40,6 +40,6 @@ pip install -r requirements-dev.txt --progress-bar off
 pip install tf-nightly --progress-bar off --upgrade
 python3 setup.py bdist_wheel
 pip uninstall -y tensorflow_gnn
-pip install dist/tensorflow_gnn-0.2.0.dev1-py3-none-any.whl
+pip install dist/tensorflow_gnn-*.whl
 
 bazel test --test_output=errors --verbose_failures=true --build_tests_only --define=no_tfgnn_py_deps=true --keep_going --experimental_repo_remote_exec //bazel_pip/tensorflow_gnn/...
