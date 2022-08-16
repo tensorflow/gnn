274cc3b263490782506b249a1942e418f263a9f1
Mihir Paradkar <mparadkar@google.com>
2022-08-16 07:15:39 -0700

    Adds license headers to source files
    PiperOrigin-RevId: 467917959

--- tensorflow_gnn/tools/print_training_data.py
+++ tensorflow_gnn/tools/print_training_data.py
@@ -1,3 +1,17 @@
+# Copyright 2021 The TensorFlow GNN Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 """Print a stream of graph tensor example protos, for inspection and debugging.
 
 You can use this to check out your data. This tool can convert the data in a
