be3506693aa9348721129371cb6dc952811a207d
Mihir Paradkar <mparadkar@google.com>
2022-09-08 06:02:38 -0700

    Fixes broken import paths in examples.
    PiperOrigin-RevId: 472967723

--- examples/in_memory/keras_trainer.py
+++ examples/in_memory/keras_trainer.py
@@ -28,9 +28,9 @@ from absl import app
 from absl import flags
 import tensorflow as tf
 import tensorflow_gnn as tfgnn
-from tensorflow_gnn.examples.in_memory import datasets
-from tensorflow_gnn.examples.in_memory import models
-from tensorflow_gnn.examples.in_memory import reader_utils
+import datasets
+import models
+import reader_utils
 
 FLAGS = flags.FLAGS
 
