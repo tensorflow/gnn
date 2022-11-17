53ed45a25c489a6fe55f6dd5bbeea4e932f55674
Parth Kothari <parthkothari@google.com>
2022-11-17 10:08:36 -0800

    Fix typo
    PiperOrigin-RevId: 489240502

--- tensorflow_gnn/runner/orchestration_test.py
+++ tensorflow_gnn/runner/orchestration_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Tests for orchestraion."""
+"""Tests for orchestration."""
 import os
 from typing import Sequence
 
