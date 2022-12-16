50efff20299f493dc94b09dd1248217f11eff81b
Parth Kothari <parthkothari@google.com>
2022-12-16 01:55:35 -0800

    Add separate kwargs for file_pattern and filenames in DatasetProvider
    PiperOrigin-RevId: 495815135

--- tensorflow_gnn/runner/input/datasets.py
+++ tensorflow_gnn/runner/input/datasets.py
@@ -82,13 +82,16 @@ class PassthruDatasetProvider:
         examples_shuffle_size=self._examples_shuffle_size)
 
 
-class SimpleDatasetProvider:
-  """Builds a `tf.data.Dataset` from a file pattern.
+class SimpleDatasetProvider(interfaces.DatasetProvider):
+  """Builds a `tf.data.Dataset` from a list of files.
 
   This `SimpleDatasetProvider` builds a `tf.data.Dataset` as follows:
-   - The filenames matching the given `file_pattern` are sharded according to
-     the `InputContext`.
-   - Filenames are shuffled (if requested).
+   - The object is initialized with a list of filenames. For convenience,
+     a file pattern can be specified instead, which will be expanded to a
+     sorted list.
+   - The filenames are sharded between replicas according to the `InputContext`
+     (order matters).
+   - Filenames are shuffled per replica (if requested).
    - The files in each shard are interleaved after being read by the
      `interleave_fn`.
    - Examples are shuffled (if requested), auto-prefetched, and returned for use
@@ -96,30 +99,43 @@ class SimpleDatasetProvider:
   """
 
   def __init__(self,
-               file_pattern: str,
+               file_pattern: Optional[str] = None,
                *,
+               filenames: Optional[Sequence[str]] = None,
                shuffle_filenames: bool = False,
                interleave_fn: Callable[..., tf.data.Dataset],
                examples_shuffle_size: Optional[int] = None):
     """Captures the args shared across `get_dataset(...)` calls.
 
     Args:
-      file_pattern: A file pattern (to be glob with `tf.io.gfile.glob`).
-      shuffle_filenames: If enabled, filenames will be shuffled before any file
-        reads. Through interleaving, some files may be read in parallel: the
-        details are auto-tuned for throughput.
+      file_pattern: A file pattern, to be expanded by `tf.io.gfile.glob`
+        and sorted into the list of all `filenames`.
+      filenames: A list of all filenames, specified explicitly.
+        This argument is mutually exclusive with `file_pattern`.
+      shuffle_filenames: If enabled, filenames will be shuffled after sharding
+        between replicas, before any file reads. Through interleaving, some
+        files may be read in parallel: the details are auto-tuned for
+        throughput.
       interleave_fn: A callback that receives a single filename and returns
         a `tf.data.Dataset` with the `tf.Example` values from that file.
       examples_shuffle_size: An optional buffer size for example shuffling.
     """
     self._file_pattern = file_pattern
+    self._filenames = filenames
+    if (self._file_pattern is not None) + (self._filenames is not None) != 1:
+      raise ValueError(
+          "Please provide either `_file_pattern` or `_filenames` argument, "
+          "but not both.")
     self._shuffle_filenames = shuffle_filenames
     self._interleave_fn = interleave_fn
     self._examples_shuffle_size = examples_shuffle_size
 
   def get_dataset(self, context: tf.distribute.InputContext) -> tf.data.Dataset:
     """Gets a `tf.data.Dataset` by `context` per replica."""
-    filenames = _sorted_glob_or_raise(self._file_pattern)
+    if self._file_pattern is not None:
+      filenames = _sorted_glob_or_raise(self._file_pattern)
+    else:
+      filenames = self._filenames
     return _process_dataset(
         tf.data.Dataset.from_tensor_slices(filenames),
         num_shards=context.num_input_pipelines,
@@ -254,15 +270,21 @@ class PassthruSampleDatasetsProvider:
         examples_shuffle_size=self._examples_shuffle_size)
 
 
-class SimpleSampleDatasetsProvider:
-  """Builds a sampling `tf.data.Dataset` from a multiple file patterns.
+class SimpleSampleDatasetsProvider(interfaces.DatasetProvider):
+  """Builds a sampling `tf.data.Dataset` from multiple filenames.
 
   For complete explanations regarding sampling see `_process_sampled_dataset()`.
 
   This `SimpleSampleDatasetsProvider` builds a `tf.data.Dataset` as follows:
-   - The filenames matching the given `principal_file_pattern` and
-     `extra_file_patterns` are sharded according to the `InputContext`.
-   - Filenames are shuffled (if requested).
+
+   - The object is initialized with a list of filenames specified by
+     `principle_filenames` and `extra_filenames` argument. For convenience,
+     the corresponding file pattern `principal_file_pattern` and
+     `extra_file_patterns` can be specified instead, which will be expanded to a
+     sorted list.
+   - The filenames are sharded between replicas according to the `InputContext`
+     (order matters).
+   - Filenames are shuffled per replica (if requested).
    - Examples from all file patterns are sampled according to `principal_weight`
      and `extra_weights.`
    - The files in each shard are interleaved after being read by the
@@ -272,11 +294,13 @@ class SimpleSampleDatasetsProvider:
   """
 
   def __init__(self,
-               principal_file_pattern: str,
-               extra_file_patterns: Sequence[str],
+               principal_file_pattern: Optional[str] = None,
+               extra_file_patterns: Optional[Sequence[str]] = None,
                principal_weight: Optional[float] = None,
                extra_weights: Optional[Sequence[float]] = None,
                *,
+               principal_filenames: Optional[Sequence[str]] = None,
+               extra_filenames: Optional[Sequence[Sequence[str]]] = None,
                principal_cardinality: Optional[int] = None,
                fixed_cardinality: bool = False,
                shuffle_filenames: bool = False,
@@ -285,21 +309,27 @@ class SimpleSampleDatasetsProvider:
     """Captures the args shared across `get_dataset(...)` calls.
 
     Args:
-      principal_file_pattern: A principal file pattern for sampling
-        (to be globed with `tf.io.gfile.glob`).
-      extra_file_patterns: File patterns (to be globed with `tf.io.gfile.glob`).
+      principal_file_pattern: A principal file pattern for sampling, to be
+        expanded by `tf.io.gfile.glob` and sorted into the list of
+        `principal_filenames`.
+      extra_file_patterns: File patterns, to be expanded by `tf.io.gfile.glob`
+        and sorted into the list of `extra_filenames`.
       principal_weight: An optional weight for the dataset corresponding to
         `principal_file_pattern.` Required iff `extra_weights` are also
         provided.
       extra_weights: Optional weights corresponding to `file_patterns` for
         sampling. Required iff `principal_weight` is also provided.
+      principal_filenames: A list of principal filenames, specified explicitly.
+        This argument is mutually exclusive with `principal_file_pattern`.
+      extra_filenames: A list of extra filenames, specified explicitly.
+        This argument is mutually exclusive with `extra_file_patterns`.
       principal_cardinality: Iff `fixed_cardinality`=True, the size of the
         returned dataset is computed as `principal_cardinality` /
         `principal_weight` (with a default of uniform weights).
       fixed_cardinality: Whether to take a fixed number of elements.
-      shuffle_filenames: If enabled, filenames will be shuffled before any file
-        reads. Through interleaving, some files may be read in parallel: the
-        details are auto-tuned for throughput.
+      shuffle_filenames: If enabled, filenames will be shuffled after sharding
+        between replicas, before any file reads. Through interleaving, some
+       files may be read in parallel: the details are auto-tuned for throughput.
       interleave_fn: A fn applied with `tf.data.Dataset.interleave.`
       examples_shuffle_size: An optional buffer size for example shuffling. If
         specified, the size is adjusted to `shuffle_size //
@@ -307,6 +337,13 @@ class SimpleSampleDatasetsProvider:
     """
     self._principal_file_pattern = principal_file_pattern
     self._extra_file_patterns = extra_file_patterns
+    self._principal_filenames = principal_filenames
+    self._extra_filenames = extra_filenames
+    if (self._principal_file_pattern is not None) + (self._principal_filenames
+                                                     is not None) != 1:
+      raise ValueError(
+          "Please provide either `_principal_file_pattern` or "
+          "`_principal_filenames` argument, but not both.")
     self._principal_weight = principal_weight
     self._extra_weights = extra_weights
     self._principal_cardinality = principal_cardinality
@@ -375,10 +412,16 @@ class SimpleSampleDatasetsProvider:
     Returns:
       A `tf.data.Dataset.`
     """
-    principal_filenames = _sorted_glob_or_raise(self._principal_file_pattern,
-                                                "principal file pattern")
-    extra_filenames = [_sorted_glob_or_raise(f, "extra file pattern")
-                       for f in self._extra_file_patterns]
+    if self._principal_file_pattern is not None:
+      principal_filenames = _sorted_glob_or_raise(self._principal_file_pattern,
+                                                  "principal file pattern")
+      extra_filenames = [
+          _sorted_glob_or_raise(f, "extra file pattern")
+          for f in self._extra_file_patterns
+      ]
+    else:
+      principal_filenames = self._principal_filenames
+      extra_filenames = self._extra_filenames
     return _process_sampled_dataset(
         tf.data.Dataset.from_tensor_slices(principal_filenames),
         [tf.data.Dataset.from_tensor_slices(f) for f in extra_filenames],
