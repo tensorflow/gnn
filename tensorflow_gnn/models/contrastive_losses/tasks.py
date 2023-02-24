f3db7f281e9b9f1f06f86593f4d83ec8d231bc23
Dustin Zelle <dzelle@google.com>
2023-02-24 14:26:30 -0800

    Update documentation.
    PiperOrigin-RevId: 512163606

--- tensorflow_gnn/models/contrastive_losses/tasks.py
+++ tensorflow_gnn/models/contrastive_losses/tasks.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Deep Graph Infomax tasks."""
+"""Contrastive loss tasks."""
 from __future__ import annotations
 
 import abc
