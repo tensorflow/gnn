ad1ce306db9e0df36803f8e9a27ebf6953dd22f5
Arno Eigenwillig <arnoegw@google.com>
2023-02-28 23:42:12 -0800

    Add HgtConvTest.test_multi_edge_set_attention.
    PiperOrigin-RevId: 513149170

--- tensorflow_gnn/models/hgt/layers_test.py
+++ tensorflow_gnn/models/hgt/layers_test.py
@@ -17,6 +17,7 @@ import enum
 import os
 
 from absl.testing import parameterized
+import numpy as np
 import tensorflow as tf
 import tensorflow_gnn as tfgnn
 from tensorflow_gnn.models.hgt import layers
@@ -73,6 +74,38 @@ def _heterogeneous_example_graph():
               ),
           )
       },
+  )
+
+
+def _parallel_vee_example_graph():
+  """Returns a graph with two parallel V-shaped edge sets."""
+  return tfgnn.GraphTensor.from_pieces(
+      node_sets={
+          "senders": tfgnn.NodeSet.from_fields(
+              sizes=[2],
+              features={tfgnn.HIDDEN_STATE: tf.constant([[1., 0.], [0., 1.]])}
+          ),
+          "receiver": tfgnn.NodeSet.from_fields(
+              sizes=[1],
+              features={tfgnn.HIDDEN_STATE: tf.constant([[1., 0.]])}
+          ),
+      },
+      edge_sets={
+          "link_a": tfgnn.EdgeSet.from_fields(
+              sizes=[2],
+              adjacency=tfgnn.Adjacency.from_indices(
+                  source=("senders", [0, 1]),
+                  target=("receiver", [0, 0]),
+              ),
+          ),
+          "link_b": tfgnn.EdgeSet.from_fields(
+              sizes=[2],
+              adjacency=tfgnn.Adjacency.from_indices(
+                  source=("senders", [0, 1]),
+                  target=("receiver", [0, 0]),
+              ),
+          ),
+      }
   )
 
 
@@ -498,6 +531,75 @@ class HgtConvTest(tf.test.TestCase, parameterized.TestCase):
         ((log20 + sqrt2) / 2) * tf.eye(4),
         rtol=1e-06,
     )
+
+  def test_multi_edge_set_attention(self):
+    """Tests uniform attention over 2 edge sets with edge-dependent weights."""
+    self._skip_if_unsupported()
+    conv = layers.HGTGraphUpdate(
+        num_heads=1,
+        per_head_channels=2,
+        receiver_tag=tfgnn.TARGET,
+        dropout_rate=0.0,
+        use_layer_norm=False,
+        use_weighted_skip=False,
+        activation="relu",
+    )
+    input_graph = _parallel_vee_example_graph()
+    _ = conv(input_graph)  # Trigger creation of weights.
+    weights = {v.name: v for v in conv.trainable_weights}
+    self.assertLen(weights, 14)  # See below, plus 4 bias terms left at zero.
+
+    # The test graph has node sets "senders" (2 nodes) and "receiver" (1 node).
+    # There are two edge sets, "link_a" and "link_b", each of which connects
+    # both senders to the receiver.
+    #
+    # The initial node-states are one-hot encodings of the node id.
+    # The receiver node is mapped trivially to the attention query [1, 0].
+    weights["hgt_graph_update/query_node_receiver/kernel:0"].assign(
+        np.array([[1., 0.], [99., 99.]]))
+    # The sender nodes are mapped to the following messages.
+    weights["hgt_graph_update/message_node_senders/kernel:0"].assign(
+        np.array([[2., 5.], [3., 7.]]))
+    # The message projections per edge set are set up such that
+    # "link_a" uses only the first component, that is [2,0] and [3,0], resp.,
+    # while "link_b" uses only the second component, [0,5] and [0,7].
+    weights["hgt_graph_update/message_edge_link_a/kernel:0"].assign(
+        np.array([[[1., 0.], [0., 0.]]]))  # Maps [x,y] to [x,0].
+    weights["hgt_graph_update/message_edge_link_b/kernel:0"].assign(
+        np.array([[[0., 0.], [0., 1.]]]))  # Maps [x,y] to [0,y].
+    # Similarly, the sender nodes get attention keys from which we extract
+    # the first component for "link_a" and the second component for "link_b" to
+    # match up with the attention query [1, 0]. The keys are chosen such that
+    # the attention weights will be  10*[2,0] + 1*[3,0] for "link_a" and
+    # 2*[0,5] + 20*[0,7] for "link_b", divided by the sum of weights 33.
+    weights["hgt_graph_update/key_node_senders/kernel:0"].assign(
+        np.log([[10., 2.], [1., 20.]]))  # Softmax will undo the log().
+    weights["hgt_graph_update/attention_edge_link_a/kernel:0"].assign(
+        np.array([[[1., 0.], [0., 0.]]]))  # Maps [x,y] to [x,0].
+    weights["hgt_graph_update/attention_edge_link_b/kernel:0"].assign(
+        np.array([[[0., 1.], [0., 0.]]]))  # Maps [x,y] to [y,0].
+    # HGT defines an additional scalar weight for use in dot-product similarity
+    # of each edge set. We simply use that to cancel out the Transformer-style
+    # division of scalar products by the square root of the dimension.
+    weights["hgt_graph_update/priors_link_a:0"].assign(np.sqrt([2.]))
+    weights["hgt_graph_update/priors_link_b:0"].assign(np.sqrt([2.]))
+    # HGT applies a feed-forward transformation on attention results.
+    # We use it to make the denominator of softmax more readable in decimal:
+    # The original weight vectors are [10, 1] / 33 for "link_a" and
+    # [2, 20] / 33 for "link_b"; they now become [1.0, 0.1] and 2*[0.1, 1.0],
+    # which leads to the result [2.3, 2*7.5].
+    # Notice how mapping [[2., 5.], [3., 7.]] to [2.3, 2*7.5] demonstrates
+    # the use of one global softmax, but with reversed attention preference
+    # between the two edge sets.
+    weights["hgt_graph_update/aggr_node_receiver/kernel:0"].assign(
+        np.array([[3.3, 0.], [0., 3.3]]))
+    # The expected final output is the result discussed above,
+    # plus the residual link from the old receiver state [1, 0].
+    expected = np.array([[1. + 2.3, 2 * 7.5]])
+
+    graph = conv(input_graph)
+    actual = graph.node_sets["receiver"][tfgnn.HIDDEN_STATE].numpy()
+    self.assertAllClose(expected, actual)
 
   @parameterized.named_parameters(
       ("NoneInitializer", ReloadModel.SKIP, None),
