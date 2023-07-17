da57c0c90f83b72dfa244b56407bdfe0f9400ed5
Parth Kothari <parthkothari@google.com>
2023-07-17 07:52:42 -0700

    Expose SamplingOp in sampler __init__.py
    PiperOrigin-RevId: 548689605

--- tensorflow_gnn/sampler/__init__.py
+++ tensorflow_gnn/sampler/__init__.py
@@ -2,7 +2,7 @@
 from tensorflow_gnn.sampler import sampling_spec_builder
 from tensorflow_gnn.sampler import sampling_spec_pb2
 
-
+SamplingOp = sampling_spec_pb2.SamplingOp
 SamplingSpec = sampling_spec_pb2.SamplingSpec
 SamplingSpecBuilder = sampling_spec_builder.SamplingSpecBuilder
 make_sampling_spec_tree = sampling_spec_builder.make_sampling_spec_tree
