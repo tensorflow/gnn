3de0749cb85da6107ea87a05f75ceef020cf411b
Oleksandr Ferludin <aferludin@google.com>
2023-10-20 09:58:46 -0700

    No public description
    PiperOrigin-RevId: 575247902

--- tensorflow_gnn/experimental/sampler/beam/executor_lib_test.py
+++ tensorflow_gnn/experimental/sampler/beam/executor_lib_test.py
@@ -210,8 +210,8 @@ class TFModelStageTest(ExecutorTestBase):
   def test_any_composite(self):
 
     # TODO: b/294854329 - Re-enable when the TF fuzzing issue is resolved.
-    if tf.__version__.startswith('2.15'):
-      self.skipTest('Fails with tf internal error on tf 2.14 nightly')
+    if tf.__version__.startswith('2.15') or tf.__version__.startswith('2.16'):
+      self.skipTest('Fails with tf internal error on tf 2.15, 2.16 nightly')
 
     i = tf.keras.Input([2], name='input')
     o = i
