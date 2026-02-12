a33b47bdc2be94251c083a3b402085cf0a199c8f
Daniel Sosa <sosagarcia@google.com>
2026-02-12 10:49:28 -0800

    Internal infrastructure change
    PiperOrigin-RevId: 869288505

--- setup.py
+++ setup.py
@@ -16,20 +16,13 @@
 
 import os
 import platform
+import shutil
 import subprocess
 import sys
-
 import setuptools
-from setuptools import find_namespace_packages
-from setuptools import setup
-from setuptools.command.install import install
-from setuptools.dist import Distribution
-# pylint:disable=g-bad-import-order
-# setuptools must be imported prior to distutils.
-# TODO(b/317036825): Stop using distutils, Python 3.12 doesn't have it.
-from distutils import spawn
-from distutils.command import build
-# pylint:enable=g-bad-import-order
+from setuptools import dist
+from setuptools.command import build
+from setuptools.command import install
 
 
 class _BuildCommand(build.build):
@@ -57,7 +50,8 @@ class _BuildCommand(build.build):
   # sequentially when running a "build" command, if the second item in the tuple
   # (predicate method) is evaluated to true.
   sub_commands = [
-      ('bazel_build', _build_cc_extensions)] + build.build.sub_commands
+      ('bazel_build', _build_cc_extensions)
+  ] + build.build.sub_commands
 
 
 class _BazelBuildCommand(setuptools.Command):
@@ -71,7 +65,7 @@ class _BazelBuildCommand(setuptools.Command):
     pass
 
   def finalize_options(self):
-    self._bazel_cmd = spawn.find_executable('bazel')
+    self._bazel_cmd = shutil.which('bazel')
     if not self._bazel_cmd:
       raise RuntimeError(
           'Could not find "bazel" binary. Please visit '
@@ -98,14 +92,14 @@ class _BazelBuildCommand(setuptools.Command):
 # TFDV is not a purelib. However because of the extension module is not built
 # by setuptools, it will be incorrectly treated as a purelib. The following
 # works around that bug.
-class _InstallPlatlibCommand(install):
+class _InstallPlatlibCommand(install.install):
 
   def finalize_options(self):
-    install.finalize_options(self)
+    install.install.finalize_options(self)
     self.install_lib = self.install_platlib
 
 
-class _SourceDistributionWithProtos(Distribution):
+class _SourceDistributionWithProtos(dist.Distribution):
   """Proto-only deps don't require OS specific wheels."""
 
   def is_pure(self):
@@ -115,96 +109,11 @@ class _SourceDistributionWithProtos(Distribution):
     return False
 
 
-def get_version():
-  """Get version from version module."""
-  version_path = os.path.join(os.path.dirname(__file__), 'tensorflow_gnn')
-  sys.path.insert(0, version_path)
-  # pytype: disable=import-error  # pylint: disable=g-import-not-at-top
-  from version import __version__ as v
-  return v
-
-
-# Get the long description from the README file.
-# TODO(b/316329189): The relative links from README.md don't work on PyPI.
-with open('README.md') as fp:
-  _LONG_DESCRIPTION = fp.read()
-
-
-console_scripts = [
-    'tensorflow_gnn.converters.ogb.convert_ogb_dataset',
-    'tensorflow_gnn.experimental.sampler.beam.sampler',
-    # copybara:uncomment_begin(NetworkX utils)
-    # 'tensorflow_gnn.sampler.nx_converter',
-    # 'tensorflow_gnn.sampler.nx_generator',
-    # copybara:uncomment_end
-    'tensorflow_gnn.tools.generate_training_data',
-    'tensorflow_gnn.tools.print_training_data',
-    'tensorflow_gnn.tools.sampled_stats',
-    'tensorflow_gnn.tools.validate_graph_schema',
-]
-
-
-setup(
-    name='tensorflow-gnn',
-    version=get_version(),
-    author='Google LLC',
-    author_email='tensorflow-gnn@googlegroups.com',
-    license='Apache 2.0',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    namespace_packages=[],
-    install_requires=[
-        'google-vizier>=0.0.13,!=0.1.23,!=0.1.24',  # b/394062744
-        'ml-collections',
-        'networkx',
-        'pyarrow',
-        # pylint:disable=g-line-too-long
-        'tensorflow>=2.12.0,<3; platform_machine != "arm64" or platform_system != "Darwin"',
-        'tensorflow-macos>=2.12.0,<3; platform_machine == "arm64" and platform_system == "Darwin"',
-        # pylint:enable=g-line-too-long
-        'apache-beam>=2.54.0',
-        'crcmod>=1.7,<2',
-    ],
-    python_requires='>=3.9,<4',
-    packages=find_namespace_packages(
-        exclude=['examples*', 'package*', 'testdata*'],
-    ),
-    include_package_data=True,
-    package_data={'': ['*.proto']},
-    zip_safe=False,
+setuptools.setup(
     distclass=_SourceDistributionWithProtos,
-    description='A library for building scalable graph neural networks in TensorFlow.',
-    long_description=_LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    keywords='tensorflow gnn graph',
-    url='https://github.com/tensorflow/gnn',
-    download_url='https://github.com/tensorflow/gnn.git',
-    requires=[],
     cmdclass={
         'install': _InstallPlatlibCommand,
         'build': _BuildCommand,
         'bazel_build': _BazelBuildCommand,
     },
-    entry_points={
-        'console_scripts': [
-            'tfgnn_{}={}:main'.format(libname.split('.')[-1], libname)
-            for libname in console_scripts
-        ],
-    }
 )
